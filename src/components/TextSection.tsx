f6553cd4fdc1db2276b894edd2970c6526d133be
Christoffer Friman <christoffer.k.friman@gmail.com>
2025-03-27 20:30:31 +0100

    Update project info

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -13,7 +13,7 @@ const TextSection: React.FC<TextSectionProps> = ({
 }) => {
   return (
     <>
-      <section className="mb-10 px-5 sm:max-w-[400px] mx-auto z-10">
+      <section className="mb-10 px-5 sm:max-w-[450px] mx-auto z-10">
         <h1 className="text-[#131313] font-bold uppercase">
           {title}{" "}
           {italicText && (
@@ -27,7 +27,7 @@ const TextSection: React.FC<TextSectionProps> = ({
         </p>
       </section>
       {children}
-      <section className="mb-10 px-5 sm:max-w-[400px] mx-auto">
+      <section className="mb-10 px-5 sm:max-w-[450px] mx-auto">
         <p className="text-sm font-light text-[#131313] sm:mt-10 tracking-wide">
           {descriptionSection2}
         </p>
